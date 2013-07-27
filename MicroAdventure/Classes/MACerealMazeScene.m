e1aeb553b32097f87955d34b2273d6794a9751de
Philipp Brendel <waldrumpus@gmail.com>
2013-07-27 19:29:43 +0200

    add: begin implementation of puzzle navigation

--- MicroAdventure/Classes/MACerealMazeScene.m
+++ MicroAdventure/Classes/MACerealMazeScene.m
@@ -10,8 +10,10 @@
 #import "GunCase.h"
 
 @interface MACerealMazeScene ()
-@property (strong, nonatomic) GCSprite *bg, *marker;
+@property (strong, nonatomic) GCSprite *bg;
+@property (strong, nonatomic) GCActor *marker;
 @property (strong, nonatomic) GCAnimation *animation;
+@property (strong, nonatomic) GCStack *path, *backtrack;
 @end
 
 @implementation MACerealMazeScene
@@ -20,8 +22,9 @@
 {
     self = [super init];
     if (self) {
+        GCSprite *markerSprite = [GCSprite spriteWithImage: [NSImage imageNamed: @"puzzle_maze_marker"]];
         self.bg = [GCSprite spriteWithImage: [NSImage imageNamed: @"puzzle_maze"]];
-		self.marker = [GCSprite spriteWithImage: [NSImage imageNamed: @"puzzle_maze_marker"]];
+        self.marker = [[GCSpriteActor alloc] initWithSprite: markerSprite];
 		
 		GCVector *startLocation = [GCVector vectorWithX: 0 y: -512];
 		
@@ -29,6 +32,11 @@
 			[GCLinearAnimation animationFrom: startLocation
 										  to: [GCVector zero]
 									duration: 0.5];
+        
+        self.path = [[GCStack alloc] init];
+        self.backtrack = [[GCStack alloc] init];
+        
+        [self loadPath];
     }
     return self;
 }
@@ -37,11 +45,28 @@
 {
 	[self.bg drawAtX: 0
 				   y: 0];
+    
+    [self.marker render];
 }
 
 - (void) update
 {
 	[self.animation advance: self];
+}
+
+- (void) loadPath
+{
+    NSString *pathString =
+        @"URUUULLDRRRUULLLLDDLLLURUU";
+    
+    [pathString enumerateSubstringsInRange: NSMakeRange(0, pathString.length)
+                                   options: NSStringEnumerationReverse | NSStringEnumerationByComposedCharacterSequences
+                                usingBlock: ^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop)
+                                            {
+                                                [self.path push: substring];
+                                            }];
+    
+    NSLog(@"Path = %@", [self.path.allObjects componentsJoinedByString: @""]);
 }
 
 @end
